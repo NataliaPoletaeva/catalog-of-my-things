2021b7ca7b3683413c263d477d8f1e9bf2ca6b12
tYohannes <teklegy@gmail.com>
2022-04-18 17:15:32 +0300

    Create Item class in item.rb file

---
+++ item.rb
@@ -0,0 +1,30 @@
+class Item
+  attr_reader :id, :archived
+  attr_accessor :publish_date
+
+  def initialize(publish_date)
+    @id = Random.rand(1..10_000)
+    @publish_date = publish_date
+    @archived = false
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
+  def source=(source)
+    @source = source
+    source.items.push(self) unless source.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+end

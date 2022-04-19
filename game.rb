cf9618f234044775c50a43658ed2183e6b61afbd
Natalia Poletaeva <natfly799@gmail.com>
2022-04-19 14:28:03 +0500

    Add Author and Game classes

---
+++ game.rb
@@ -0,0 +1,11 @@
+require_relative './item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_date, :publish_date
+
+  def initialize(multiplayer, last_played_date, publish_date)
+    super(name, publish_date)
+    @last_played_date = last_played_date
+    @multiplayer = multiplayer
+  end
+end

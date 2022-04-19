9969dd8a36bcb29e580c5d75edcabc41f4adacc7
Natalia Poletaeva <natfly799@gmail.com>
2022-04-19 16:23:21 +0500

    Delete name from constructor

--- game.rb
+++ game.rb
@@ -4,7 +4,7 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_date, :publish_date
 
   def initialize(multiplayer, last_played_date, publish_date)
-    super(name, publish_date)
+    super(publish_date)
     @last_played_date = last_played_date
     @multiplayer = multiplayer
   end

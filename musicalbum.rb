c8b506881c1c11ef0a2546ec57ed71df5d625d8a
mrparkersson <amoparkerprince78@gmail.com>
2022-04-20 08:04:43 +0000

    Add publish date as part of initialize

--- musicalbum.rb
+++ musicalbum.rb
@@ -3,7 +3,7 @@ require './item'
 class Musicalbum < Item
   attr_accessor :on_spotify
 
-  def initialize(on_spotify,publish_date)
+  def initialize(on_spotify, publish_date)
     super(publish_date)
     @on_spotify = on_spotify
   end

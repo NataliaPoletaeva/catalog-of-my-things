bcdabb190ce2ba5100355a75aeb43483f51b64f9
tYohannes <teklegy@gmail.com>
2022-04-20 11:59:46 +0300

    Add list_books and list_labels methods to App class

--- app.rb
+++ app.rb
@@ -8,4 +8,17 @@ class App
     @books = []
     @labels = []
   end
+
+  def list_books
+    puts 'No books are available currently!' if @books.empty?
+    @books.each_with_index do |book, indx|
+      puts "#{indx + 1} ) [Book] Id: #{book.id}, publisher: #{book.publisher}, cover-state: #{book.cover_state}
+            Label: title: #{book.label.title} color: #{book.label.color}"
+    end
+  end
+
+  def list_labels
+    puts 'No labels are available currently!'
+    @labels.each_with_index { |label, indx| p indx + 1, label }
+  end
 end

18a74ef271abe222bb8e03cc0b4f39b726b3e5f9
tYohannes <teklegy@gmail.com>
2022-04-20 12:00:43 +0300

    Add create_book and create_label methods to App class

--- app.rb
+++ app.rb
@@ -21,4 +21,46 @@ class App
     puts 'No labels are available currently!'
     @labels.each_with_index { |label, indx| p indx + 1, label }
   end
+
+  def create_label
+    user_input = label_input
+    label = Label.new(user_input[:title], user_input[:color])
+    @labels << label
+    label
+  end
+
+  def add_label_to_item
+    puts 'Add label:
+      1 - select from existing labels
+      2 - create new label'
+    label_source = gets.chomp
+    label = nil
+    case label_source
+    when '1'
+      list_labels
+      if @labels.empty?
+        puts 'No labels avail. Please, create label!'
+        label = create_label
+      else
+        print 'Enter the number on the left to select a label: '
+        label_index = gets.chomp.to_i
+        label = @labels[label_index - 1]
+      end
+    when '2' then label = create_label
+    end
+    label
+  end
+
+  def create_book
+    user_input = book_input
+    label = add_label_to_item
+    book = Book.new(
+      user_input[:publisher],
+      user_input[:publish_date],
+      user_input[:cover_state]
+    )
+    label&.add_item(book)
+    @books << book
+    book
+  end
 end

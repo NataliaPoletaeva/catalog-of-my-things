3690ee3e1768cc7fc0d06bc8ea0a9efcdc5d4b36
mrparkersson <amoparkerprince78@gmail.com>
2022-04-21 15:20:16 +0000

    Add schema for music and genre

--- db/schema.sql
+++ db/schema.sql
@@ -19,3 +19,21 @@ CREATE TABLE labels (
   title varchar(100),
   color varchar(100)
 );
+
+
+-- add schema for music and genres
+
+CREATE TABLE genres (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE music_albums (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN,
+    on_spotify BOOLEAN NOT NULL,
+    genre_id INT,
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
+);

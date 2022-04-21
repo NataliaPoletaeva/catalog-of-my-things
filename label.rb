42b24770d0084832034e3380ee62f1fabc4caffb
tYohannes <teklegy@gmail.com>
2022-04-21 11:34:48 +0300

    Add to_json() and json_create() methods to the Label class to handle ruby object to JSON conversion operations

--- label.rb
+++ label.rb
@@ -4,8 +4,12 @@ class Label
   attr_accessor :title, :color
   attr_reader :items, :id
 
-  def initialize(title, color)
-    @id = Random.rand(1..10_000)
+  def initialize(title, color, id = -1)
+    @id = if id == -1
+            Random.rand(1..10_000)
+          else
+            id
+          end
     @title = title
     @color = color
     @items = []
@@ -14,5 +18,22 @@ class Label
   def add_item(item)
     @items.push(item)
     item.label = self
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'label' => {
+        'title' => @title,
+        'color' => @color,
+        'id' => @id
+      }
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    object_variables = []
+    object['label'].each { |_, value| object_variables.push(value) }
+    new(*object_variables)
   end
 end

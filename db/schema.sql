f897921a29c25ca6021c1874756d33f1fde596c5
Natalia Poletaeva <natfly799@gmail.com>
2022-04-22 12:33:59 +0500

    Add schema for the authors adnd games

--- db/schema.sql
+++ db/schema.sql
@@ -9,8 +9,26 @@ CREATE TABLE books (
   cover_state varchar(50),
   archived boolean,
   label_id integer NOT NULL,
-  CONSTRAINT fk_Labels  
+  author_id integer NOT NULL,
+  CONSTRAINT fk_Labels
+  CONSTRAINT fk_Authors 
   FOREIGN KEY(label_id) REFERENCES labels(id)
+  FOREIGN KEY(author_id) REFERENCES authors(id)
+);
+
+-- create games table
+CREATE TABLE games (
+  id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  multiplayer boolean,
+  last_played_date date,
+  publish_date date,
+  archived boolean,
+  label_id integer NOT NULL,
+  author_id integer NOT NULL,
+  CONSTRAINT fk_Labels
+  CONSTRAINT fk_Authors
+  FOREIGN KEY(label_id) REFERENCES labels(id)
+  FOREIGN KEY(author_id) REFERENCES authors(id)
 );
 
 -- create labels table
@@ -18,4 +36,11 @@ CREATE TABLE labels (
   id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   title varchar(100),
   color varchar(100)
+);
+
+-- create authors table 
+CREATE TABLE authors (
+  id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name varchar(100),
+  last_name varchar(100)
 );

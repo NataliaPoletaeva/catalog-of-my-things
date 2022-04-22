c08a9d2c1ce6b0bbcf16afa1b275bc1753eaface
mrparkersson <amoparkerprince78@gmail.com>
2022-04-22 09:09:10 +0000

    Can be archived music test

--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -20,13 +20,13 @@ describe Musicalbum do
     it 'with on spotify false and new music' do
       music = Musicalbum.new(Time.local('2022-05-05'), false)
       music.move_to_archive
-      expect(music.archived).to be_falthy
+      expect(music.archived).to be_falsey
     end
 
-    it 'with book newer than 10 years but \'bad\' cover_state, should set archived property true' do
-      book = Book.new('Printice Hall', Time.local('2015-05-05'), 'bad')
-      book.move_to_archive
-      expect(book.archived).to be_truthy
+    it 'with on spotify true and new music' do
+      music = Musicalbum.new(Time.local('2022-05-05'), true)
+      music.move_to_archive
+      expect(music.archived).to be_falsey
     end
   end
 end
